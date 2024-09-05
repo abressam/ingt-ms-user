d279b8241f80bd54f4ebf373f08bcf943a2b5ec5
abressam <amandabressam@gmail.com>
2024-09-05 09:58:10 -0300

    feat: fix path in user service

--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -5,7 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { DeleteUserResDto } from '@app/modules/user/dtos/responses/delete-user-res.dto';
 import { GetUserResDto } from '@app/modules/user/dtos/responses/get-user-res.dto';
-import { PostUserReqDto } from '../dtos/requests/post-user-req.dto';
+import { PostUserReqDto } from '@app/modules/user/dtos/requests/post-user-req.dto';
 import { PutUserReqDto } from '@app/modules/user/dtos/requests/put-user-req.dto';
 import { UserDto } from '@app/modules/user/dtos/user.dto';
 import { User } from '@app/modules/user/models/user.model';
